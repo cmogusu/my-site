5cbe6dc56e2bd2f5e8a6798f502a4ce603fe0823
Clive <clive.m@turing.com>
2020-03-06 01:22:44 +0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,28 @@
 import Head from 'next/head'
+// import underConstruction from '../assets/images/under-construction.jpg';
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Hi, I'm a developer</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
+      <img className="logo" alt="clive mogusu logo" src="/logo.png" />
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Hi, I'm Clive.
       </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
+      <h2>
+         I'm a developer and I love to code in Javascript.
+      </h2>
+      <img className="construction-img" alt="site under construction" src="/under-construction.jpg" />
+      <p>
+        I'm building this site so hold on to your pants! Great things coming up.
       </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
     </main>
 
     <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
     </footer>
 
     <style jsx>{`
@@ -65,6 +33,15 @@ const Home = () => (
         flex-direction: column;
         justify-content: center;
         align-items: center;
+      }
+
+      .construction-img {
+        width: 250px;
+      }
+
+      .logo {
+        width: 96px;
+        margin: 44px 0;
       }
 
       main {
